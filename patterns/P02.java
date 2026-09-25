c2e3490c2dc618d5d279e31afd0431d1b9267d31
SK <devanshskofficial@gmail.com>
2026-09-26 00:50:27 +0530

    Add 2nd rectangle pattern

---
+++ patterns/P02.java
@@ -0,0 +1,24 @@
+public class P02
+{
+    static void main()
+    {
+        int n = 3;
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=5;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+
+* * * * *
+* * * * *
+* * * * *
+
+*/
