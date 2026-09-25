d9f99503e395475ed37eddbb51a48c1a9b1c8402
SK <devanshskofficial@gmail.com>
2026-09-26 00:19:37 +0530

    Add first square pattern

---
+++ patterns/P01.java
@@ -0,0 +1,25 @@
+public class P01
+{
+    static void main()
+    {
+        int n = 4;
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+
+* * * * 
+* * * * 
+* * * * 
+* * * * 
+
+*/
